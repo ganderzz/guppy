a472fb6809d646c575c87c882e9fad754420b66a
Dylan Paulus <dylantpaulus@gmail.com>
2018-07-15 22:49:54 -0700

    Initial pass at adding electron-builder
    Fix up configs
    
    Remove Binaries

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 const childProcess = require('child_process');
+const log = require('electron-log');
+const { autoUpdater } = require('electron-updater');
 
 const fixPath = require('fix-path');
 const psTree = require('ps-tree');
@@ -16,6 +18,43 @@ const psTree = require('ps-tree');
 if (process.env.NODE_ENV !== 'development') {
   fixPath();
 }
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', info => {
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', info => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', err => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', progressObj => {
+  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message =
+    log_message +
+    ' (' +
+    progressObj.transferred +
+    '/' +
+    progressObj.total +
+    ')';
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on('update-downloaded', info => {
+  sendStatusToWindow('Update downloaded');
+});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -45,6 +84,7 @@ function createWindow() {
       protocol: 'file:',
       slashes: true,
     });
+
   mainWindow.loadURL(startUrl);
 
   // Emitted when the window is closed.
@@ -59,7 +99,13 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', function() {
+  autoUpdater.checkForUpdatesAndNotify().then(p => {
+    log.info(p.versionInfo, p.updateInfo);
+  });
+
+  createWindow();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
